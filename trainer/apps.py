90899442bdaa456a7865aaeaa6cb3a5fc2fe27e9
DanteImp <94982437+DanteImp@users.noreply.github.com>
2025-06-27 22:38:29 +0300

    Update apps.py

--- trainer/apps.py
+++ trainer/apps.py
@@ -1,11 +1,16 @@
 from django.apps import AppConfig
 from django.db import connection
+from django.db.models.signals import post_migrate
+
 
 class TrainerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trainer'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "trainer"
 
     def ready(self):
-        with connection.cursor() as cursor:
-            cursor.execute("PRAGMA journal_mode=WAL;")
+        # migrate bittikten sonra çalışacak
+        def enable_wal(sender, **kwargs):
+            with connection.cursor() as cur:
+                cur.execute("PRAGMA journal_mode=WAL;")
 
+        post_migrate.connect(enable_wal, sender=self)
